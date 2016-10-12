642319671d5a7bb18e5b799b0b5e05742b5f4d2f
carl.duevel <carl.duevel@holisticon.de>
2016-10-12 08:06:45 +0200

    Updated deps

--- project.clj
+++ project.clj
@@ -5,12 +5,12 @@
             :url  "http://www.apache.org/license/LICENSE-2.0.html"}
   :scm {:name "git"
         :url  "https://github.com/otto-de/tesla-jsonhome"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]]
   :lein-release {:scm        :git
                  :deploy-via :shell
                  :shell      ["lein" "deploy" "clojars"]}
-  :profiles {:provided {:dependencies [[de.otto/tesla-microservice "0.1.25"]
+  :profiles {:provided {:dependencies [[de.otto/tesla-microservice "0.3.36"]
                                        [com.stuartsierra/component "0.3.1"]]}
              :dev      {:dependencies [[ring-mock "0.1.5"]]
-                        :plugins      [[lein-ancient "0.6.8"] [lein-release/lein-release "1.0.9"]]}})
+                        :plugins      [[lein-ancient "0.6.10"] [lein-release/lein-release "1.0.9"]]}})
